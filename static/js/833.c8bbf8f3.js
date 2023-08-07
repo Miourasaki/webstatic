c3d9bb82c1b8905d6f20d975e5b369df9843c115
lolilin <me@1oli.cn>
2023-08-07 11:00:56 +0800

    Merge branch 'pages' of https://github.com/lolilin/webstatic into pages

---
+++ static/js/833.c8bbf8f3.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkmiochyan"]=self["webpackChunkmiochyan"]||[]).push([[833],{833:function(n,a,u){u.r(a),u.d(a,{default:function(){return f}});var t=u(3396);const c={class:"about"},e=(0,t._)("h1",null,"This is an about page",-1),r=[e];function s(n,a){return(0,t.wg)(),(0,t.iD)("div",c,r)}var i=u(89);const o={},h=(0,i.Z)(o,[["render",s]]);var f=h}}]);
+//# sourceMappingURL=833.c8bbf8f3.js.map
